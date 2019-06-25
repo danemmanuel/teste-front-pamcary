95dd01e4f9265676047bec0baf070a69409725c3
Dan Emmanuel Vieira <danemmanuelturibio@gmail.com>
2019-06-24 22:18:58 -0300

    formulario cadastrar mensagem

--- src/app/_modules/mensagens/cadastrar-mensagem/cadastrar-mensagem.component.ts
+++ src/app/_modules/mensagens/cadastrar-mensagem/cadastrar-mensagem.component.ts
@@ -8,19 +8,34 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 })
 export class CadastrarMensagemComponent implements OnInit {
   formulario: FormGroup;
+  assuntos = [];
+  submitted: boolean;
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.buscarAssuntos();
     this.montarFormulario();
+  }
+
+  buscarAssuntos() {
+    this.assuntos = ['Orçamento', 'Dúvida', 'Elogio', 'Reclamação'];
   }
 
   montarFormulario() {
     this.formulario = this.formBuilder.group({
-      nome: [null, Validators.required],
+      nome: [null, [Validators.required, Validators.pattern('^[a-zA-Z ]+')]],
       email: [null, [Validators.required, Validators.email]],
-      senha: [null, [Validators.required]],
       telefone: [null, [Validators.required]],
-      whatsapp: [null, [Validators.required]]
+      assunto: [null, [Validators.required]],
+      mensagem: [null, [Validators.required]]
     });
+  }
+
+  submitFormulario() {
+    this.submitted = true;
+    if (this.formulario.invalid) {
+      return;
+    }
+    console.log(this.formulario);
   }
 }
